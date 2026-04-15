d36a4cefb9d6fc33e19af13117df9f22e9efdb75
mingkaikan <mingkaikan@gmail.com>
2026-04-15 17:01:15 +0800

    feat: initial portal setup for swaikaikai

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 如果你用的是 v4 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 確保 tailwind v4 外掛有在這裡
+  ],
+  base: '/', // 👈 個人主頁這裡必須是 '/'，絕對不能寫 repo 名字
 })
